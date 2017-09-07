2c3cb09236b71bf113130c1dab4d208a980b7030
Auliakukuhs <kukuhaulias@gmail.com>
2017-09-07 21:25:35 +0700

    Menambahkan tugas pertemuan 5

---
+++ 05_C2_11514/nomor2.php
@@ -0,0 +1,9 @@
+<?php
+//Auliakukuhsaputra_C2_11514
+  $x = 1;
+  $jumlah=0;
+  for ($x=1; $x < 100 ; $x+=2) {
+    $jumlah++;
+  }
+  echo "Banyaknya bilangan ganjil dari 1-100 : $jumlah ";
+?>
---
+++ 05_C2_11514/nomor3.php
@@ -0,0 +1,7 @@
+<?php
+//Auliakukuhsaputra_C2_11514
+echo "Bilangan kelipatan 7 kurang dari 100 adalah : <br>";
+  for ($x=7; $x < 100 ; $x+=7){
+      echo "$x<br>";
+    }
+?>
