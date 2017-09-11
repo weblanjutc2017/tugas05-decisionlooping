7fa76a9e3bd09b5cbb86f46f4e99fb278ce86fb7
rafikasyaf <rafika1504@gmail.com>
2017-09-11 19:27:49 +0700

    update, tugas 5 Rafika Syafaatus S

--- 05_C2_11556/4.php
+++ 05_C2_11556/4.php
@@ -4,26 +4,26 @@ $status;
 $income;
 $tax;
 $income = 75000;
-$status = "menikah";
+$status = "married";
 
-if ($status=="single"){
+if ($status=="single" && $income>=0){
 	if ($income <= 32000){
 		$tax = $income * 0.1;
 	}
 	else{
 		$tax = 3200+(0.25 * $income);
 	}
-	echo "John yang berstatus ". $status ." dan memiliki penghasilan sebesar $" . $income . ", maka pajak yang harus dibayarkan adalah sebesar $" . $tax . ".";
+	echo "Status: ". $status ."<br/>Income: $" . $income . "<br/>Tax: $" . $tax;
 }
 
-else if ($status=="menikah"){
+else if ($status=="married" && $income>=0){
 	if ($income <= 64000){
 		$tax = $income * 0.1;
 	}
 	else{
 		$tax = 6400+(0.25 * $income);
 	}
-	echo "John yang berstatus ". $status ." dan memiliki penghasilan sebesar $" . $income . ", maka pajak yang harus dibayarkan adalah sebesar $" . $tax . ".";
+	echo "Status: ". $status ."<br/>Income: $" . $income . "<br/>Tax: $" . $tax;
 }
 
 else {
